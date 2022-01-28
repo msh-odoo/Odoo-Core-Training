5cd77e569138a7614751e629d442827aa3a5291e
Mohammed Shekha <msh@odoo.com>
2022-01-28 17:48:35 +0530

    [IMP] open_academy: add compute and related field

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -5,8 +5,16 @@ class Course(models.Model):
 
     name = fields.Char(string="Name of the Course", help="This contains course name", copy=False, required=True, translate=True)
     description = fields.Html(readonly=True, states={'new': [('readonly', False)]})
-    state = fields.Selection([('new', 'New'), ('confirm', 'Confirm'), ('done', 'Done')], default="new")
+    state = fields.Selection([('new', 'New'), ('confirm', 'Confirm'), ('cancel', 'Cancel')], default="new")
     active = fields.Boolean(default=True)
 
     session_ids = fields.One2many('course.session', 'course_id', string="Sessions")
 
+    def action_confirm(self):
+        self.state
+        return True
+
+    def write(self, vals):
+        for record in self:
+            print ("\n\nrecord name is ::: ", record.name)
+        return super().write(vals)
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -1,10 +1,29 @@
+from dateutil.relativedelta import relativedelta
 from odoo import _, api, fields, models
 
 class Session(models.Model):
     _name = "course.session"
+
+    @api.depends('start_datetime', 'end_datetime')
+    def _get_duration(self):
+        for record in self:
+            if record.start_datetime and record.end_datetime:
+                diff = record.end_datetime - record.start_datetime
+                record.duration = diff.days
+
+    def _inverse_duration(self):
+        for record in self:
+            record.end_datetime = record.start_datetime + relativedelta(days=record.duration)
+
+    def _search_duration(self, operator, value):
+        self.env.cr.execute("SELECT id from course_session where end_datetime::DATE - start_datetime::DATE %s %s" %(operator, value))
+        datas = self.env.cr.fetchall()
+        return [('id', 'in', [data[0] for data  in datas])]
 
     name = fields.Char()
     start_datetime = fields.Datetime()
     end_datetime = fields.Datetime()
     course_id = fields.Many2one('course.course', string="Course")
     attendee_ids = fields.Many2many('res.partner', striing="Attendees")
+    duration = fields.Float(compute="_get_duration", inverse="_inverse_duration", search="_search_duration")
+    course_name = fields.Char(related="course_id.name")

a12e1d9194a937e1e2b5630e6b2974a3cef8a542
Mohammed Shekha <msh@odoo.com>
2022-02-14 18:28:15 +0530

    [IMP] open_academy: adds controllers and qweb templates

--- open_academy/controllers/main.py
+++ open_academy/controllers/main.py
@@ -11,4 +11,48 @@ class OpenAcademy(http.Controller):
     def hello_usr(self, **kw):
         return "Hello %s" %(request.env.user.name)
 
-    
+    @http.route('/my_template', auth="public")
+    def my_template(self, **kW):
+        return request.render("open_academy.my_template", {})
+
+    @http.route('/my_template_user', auth="user")
+    def my_template_user(self, **kw):
+        return request.render("open_academy.my_template_user", {
+            'name': request.env.user.name
+        })
+
+    @http.route('/qweb_directives', auth="user")
+    def qweb_directives(self, **kw):
+        courses = request.env['course.course'].search([])
+        return request.render("open_academy.qweb_template_example", {
+            'courses': courses,
+            'colors': {
+                0: 'green',
+                1: '#000000',
+                2: 'red',
+                3: 'grey',
+            }
+        })
+
+    @http.route('/test_tcall', auth="user")
+    def test_call(self, **kw):
+        return request.render('open_academy.test_tcall', {})
+
+    @http.route('/web_page', auth="user", website=True)
+    def web_page(self, **kw):
+        courses = request.env['course.course'].search([])
+        return request.render("open_academy.web_page", {
+            'courses': courses,
+            'colors': {
+                0: 'green',
+                1: '#000000',
+                2: 'red',
+                3: 'grey',
+            }
+        })
+
+    @http.route('/course_details/<model("course.course"):course>', auth="user", website=True)
+    def course_details(self, course=None, **kw):
+        return request.render("open_academy.course_details", {
+            'course': course,
+        })
